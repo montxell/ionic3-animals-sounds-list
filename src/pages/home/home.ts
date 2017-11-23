df25e1b59dba7fd6be6cdcb8539677102ac6cc2c
Montse Ariso <montxelltech@gmail.com>
2017-11-23 02:57:29 +0100

    Add audio controls: methods and icons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,8 +12,14 @@ import { Animal } from '../../interfaces/animal.interface';
 export class HomePage {
 
   // animals array is of the type Animal. We made an interface so we
-  // can see and have a control of all the properties (for example: this.animals[0].audio)
+  // can see and have a control of all the properties (for example:
+  // this.animals[0].audio)
   animals:Animal[] = [];
+
+  // new Audio is an object by default of html5
+  audio = new Audio();
+
+  audioTime: any;
 
 
   constructor() {
@@ -21,6 +27,49 @@ export class HomePage {
     // Clone ANIMALS so when we delete or edit the list it won't change
     // the original data (data.animals.ts)
     this.animals = ANIMALS.slice(0);
+
+  }
+
+
+  playAudio( animal:Animal ) {
+
+    this.pauseAudio( animal );
+
+    if( animal.playing ) {
+      animal.playing = false;
+      return;
+    }
+
+    // audio has the property "src" that it's the origin of the audio
+    this.audio.src = animal.audio;
+
+    // audio properties by default
+    this.audio.load();
+    this.audio.play();
+
+    // set property of playing to true
+    animal.playing = true;
+
+    // control audio: set playing to false based on the duration (duration set to milliseconds)
+    this.audioTime = setTimeout( () => animal.playing = false, animal.duration * 1000 );
+
+  }
+
+
+  private pauseAudio( selectedAnimal: Animal ) {
+
+    clearTimeout( this.audioTime );
+
+    // audio properties by default
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for( let animal of this.animals ) {
+
+      if( animal.name != selectedAnimal.name ) {
+        animal.playing = false;
+      }
+    }
 
   }
 
